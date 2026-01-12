ce86ef352c18590ee557ca2b0aa39b5e72351fad
Travis Martinez <travisesteban91@gmail.com>
2026-01-12 15:24:34 -0500

    Jitter Fix
    Fixing jitter between camera position and player model
    - CameraPitchController.cs
    - Refactor: Changed callback from FixedUpdate to LateUpdate
    
    - Player scene
    - Refactor: Changed Camera child update method to LateUpdate

--- Assets/Scripts/Player/CameraPitchController.cs
+++ Assets/Scripts/Player/CameraPitchController.cs
@@ -6,12 +6,12 @@ public class CameraPitchController : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController;
 
-    void FixedUpdate()
+    void LateUpdate()
     {
         if (!playerController.hasFocus) return;
         Vector2 rotationInput = playerController.playerControls.Movement.Look.ReadValue<Vector2>();
         Vector3 rotation = new Vector3(-rotationInput.y, 0f, 0f);
-        rotation *= playerController.verticalRotationSpeed * Time.fixedDeltaTime;
+        rotation *= playerController.verticalRotationSpeed * Time.deltaTime;
         rotation = Vector3.ClampMagnitude(rotation, playerController.verticalRotationLimit);
         float currentXRotation = transform.eulerAngles.x < 180f ? transform.eulerAngles.x : transform.eulerAngles.x - 360f;
         rotation.x = Mathf.Clamp(currentXRotation + rotation.x, -89.0f, 89.0f) - currentXRotation;
